3973098d9ca556789da9c0bf84ce2fc1dd4ebdc2
Avinash Kumar Pal <palavinashkumar6@gmail.com>
2026-08-18 15:18:02 +0530

    Added Move All Negative Elements To End - Uncategorized - Unknown

---
+++ DSA/Uncategorized/Move_All_Negative_Elements_To_End/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void segregateElements(vector<int>& arr) {
+        // code here
+        vector<int>ans;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>0){
+                ans.push_back(arr[i]);
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<0){
+                ans.push_back(arr[i]);
+            }
+        }
+        
+        arr = ans;
+    }
+};
