878134e43765ce7657d2985a5f2c881278d7b84b
Avinash Kumar Pal <palavinashkumar6@gmail.com>
2026-08-19 12:47:41 +0530

    Added Who Will Win - Binary Search - Unknown

---
+++ DSA/Binary Search/Who_Will_Win/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def binarySearch(self, arr, k):
+        # code here
+        
+        low = 0
+        high = len(arr) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if arr[mid] == k:
+                return True
+
+            elif arr[mid] < k:
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return False
